940d89e282eba5b1f0eede690ab93a6a39c4af7d
JeanLamego <jeanlamego@hotmail.com>
2023-08-26 21:00:52 -0300

    Implement first activity(basic behavior)

--- app/src/main/java/com/salvatoreacademy/firstapp/MainActivity.kt
+++ app/src/main/java/com/salvatoreacademy/firstapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.salvatoreacademy.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btSend = findViewById<Button>(R.id.btSend)
+        val tvResult = findViewById<TextView>(R.id.tvResult)
+
+        btSend.setOnClickListener {
+            tvResult.text = "Jean Lamego";
+
+        }
     }
 }
